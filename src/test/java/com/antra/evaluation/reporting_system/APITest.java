b189e160ae804252879ea8bd48f63a18e77ed6e8
ZixiaoL <zl2489@nyu.edu>
2020-09-18 20:21:48 -0400

    docs: unit test and partial integration test

--- src/test/java/com/antra/evaluation/reporting_system/APITest.java
+++ src/test/java/com/antra/evaluation/reporting_system/APITest.java
@@ -1,12 +1,14 @@
 package com.antra.evaluation.reporting_system;
 
 import com.antra.evaluation.reporting_system.endpoint.ExcelGenerationController;
+import com.antra.evaluation.reporting_system.exception.ExcelFormatException;
+import com.antra.evaluation.reporting_system.pojo.api.ExcelRequest;
+import com.antra.evaluation.reporting_system.pojo.report.ExcelFile;
 import com.antra.evaluation.reporting_system.service.ExcelService;
 import io.restassured.http.ContentType;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -14,8 +16,13 @@ import org.mockito.MockitoAnnotations;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
 public class APITest {
@@ -31,26 +38,152 @@ public class APITest {
     @Test
     public void testFileDownload() throws FileNotFoundException {
         Mockito.when(excelService.getExcelBodyById(anyString())).thenReturn(new FileInputStream("temp.xlsx"));
-        given().accept("application/json").get("/excel/123abcd/content").peek().
+        given().accept("application/json").get("/excel/temp/content").peek().
                 then().assertThat()
                 .statusCode(200);
     }
 
     @Test
-    public void testListFiles() throws FileNotFoundException {
-       // Mockito.when(excelService.getExcelBodyById(anyString())).thenReturn(new FileInputStream("temp.xlsx"));
-        given().accept("application/json").get("/excel").peek().
+    public void testFileDownloadWithInvalidId() {
+        Mockito.when(excelService.getExcelBodyById(anyString())).thenReturn(null);
+        given().accept("application/json").get("/excel/123abc/content").peek().
                 then().assertThat()
-                .statusCode(200);
+                .statusCode(400)
+                .body("message", Matchers.equalTo("file not exists"));
     }
 
     @Test
-    @Disabled
-    public void testExcelGeneration() throws FileNotFoundException {
-        // Mockito.when(excelService.getExcelBodyById(anyString())).thenReturn(new FileInputStream("temp.xlsx"));
-        given().accept("application/json").contentType(ContentType.JSON).body("{\"headers\":[\"Name\",\"Age\"], \"data\":[[\"Teresa\",\"5\"],[\"Daniel\",\"1\"]]}").post("/excel").peek().
+    public void testFileDeletion() {
+        ExcelFile savedExcelFile = new ExcelFile();
+        savedExcelFile.setId("temp");
+        Mockito.when(excelService.deleteRequest(anyString())).thenReturn(savedExcelFile);
+        given().accept("application/json").delete("/excel/temp").peek().
                 then().assertThat()
                 .statusCode(200)
-                .body("fileId", Matchers.notNullValue());
+                .body("fileId", Matchers.equalTo("temp"));
+    }
+
+    @Test
+    public void testFileDeletionWithInvalidId() {
+        Mockito.when(excelService.deleteRequest(anyString())).thenReturn(null);
+        given().accept("application/json").delete("/excel/123abc").peek().
+                then().assertThat()
+                .statusCode(400)
+                .body("message", Matchers.equalTo("file not exists"));
+    }
+
+    @Test
+    public void testListFiles() {
+        List<ExcelFile> excelFiles = new ArrayList<>();
+        ExcelFile savedExcelFile1 = new ExcelFile();
+        savedExcelFile1.setId("1");
+        ExcelFile savedExcelFile2 = new ExcelFile();
+        savedExcelFile2.setId("2");
+        excelFiles.add(savedExcelFile1);
+        excelFiles.add(savedExcelFile2);
+        Mockito.when(excelService.getAllFiles()).thenReturn(excelFiles);
+        given().accept("application/json").get("/excel").peek().
+                then().assertThat()
+                .statusCode(200)
+                .body("fileId", Matchers.hasItems("1","2"));
+    }
+
+
+    @Test
+    public void testExcelGeneration() throws IOException {
+        ExcelFile savedExcelFile = new ExcelFile();
+        savedExcelFile.setId("1");
+        Mockito.when(excelService.saveRequest(any())).thenReturn(savedExcelFile);
+        given().accept("application/json").contentType(ContentType.JSON)
+                .body("{\"headers\":[\"Name\",\"Age\"], \"data\":[[\"Teresa\",\"5\"],[\"Daniel\",\"1\"]]}")
+                .post("/excel").peek().
+                then().assertThat()
+                .statusCode(200)
+                .body("fileId", Matchers.equalTo(savedExcelFile.getId()));
+    }
+
+
+    @Test
+    public void testMultiSHeetExcelGeneration() throws IOException {
+        ExcelFile savedExcelFile = new ExcelFile();
+        savedExcelFile.setId("1");
+        Mockito.when(excelService.saveMultiSheetRequest(any())).thenReturn(savedExcelFile);
+        given().accept("application/json").contentType(ContentType.JSON)
+                .body("{\"headers\":[\"Name\",\"Age\"], \"data\":[[\"Teresa\",\"5\"],[\"Daniel\",\"1\"]], \"splitBy\":\"Age\"}")
+                .post("/excel/auto").peek().
+                then().assertThat()
+                .statusCode(200)
+                .body("fileId", Matchers.equalTo(savedExcelFile.getId()));
+    }
+
+    @Test
+    public void testMultiSHeetExcelGenerationWithNoSplitBy() throws IOException {
+        ExcelFile savedExcelFile = new ExcelFile();
+        savedExcelFile.setId("1");
+        Mockito.when(excelService.saveMultiSheetRequest(any())).thenReturn(savedExcelFile);
+        given().accept("application/json").contentType(ContentType.JSON)
+                .body("{\"headers\":[\"Name\",\"Age\"], \"data\":[[\"Teresa\",\"5\"],[\"Daniel\",\"1\"]]}")
+                .post("/excel/auto").peek().
+                then().assertThat()
+                .statusCode(400)
+                .body("message", Matchers.equalTo("splitBy cannot be blank"));
+    }
+
+    @Test
+    public void testMultiSHeetExcelGenerationWithSplitByNotInHeaders() throws IOException {
+        ExcelFile savedExcelFile = new ExcelFile();
+        savedExcelFile.setId("1");
+        Mockito.when(excelService.saveMultiSheetRequest(any())).thenReturn(savedExcelFile);
+        given().accept("application/json").contentType(ContentType.JSON)
+                .body("{\"headers\":[\"Name\",\"Age\"], \"data\":[[\"Teresa\",\"5\"],[\"Daniel\",\"1\"]], \"splitBy\":\"Gender\"}")
+                .post("/excel/auto").peek().
+                then().assertThat()
+                .statusCode(400)
+                .body("message", Matchers.equalTo("splitBy should be one of the headers"));
+    }
+
+    @Test
+    public void testExcelGenerationWithEmptyHeader() throws IOException {
+        ExcelFile savedExcelFile = new ExcelFile();
+        savedExcelFile.setId("1");
+        Mockito.when(excelService.saveRequest(any())).thenReturn(savedExcelFile);
+        given().accept("application/json").contentType(ContentType.JSON).body("{\"data\":[[\"Teresa\",\"5\"],[\"Daniel\",\"1\"]]}").post("/excel").peek().
+                then().assertThat()
+                .statusCode(400)
+                .body("errorCode", Matchers.equalTo(400))
+                .body("message", Matchers.equalTo("headers cannot be empty"));
+    }
+
+    @Test
+    public void testExcelGenerationWithWrongUrl() throws IOException {
+        ExcelFile savedExcelFile = new ExcelFile();
+        savedExcelFile.setId("1");
+        Mockito.when(excelService.saveRequest(any())).thenReturn(savedExcelFile);
+        given().accept("application/json").contentType(ContentType.JSON)
+                .body("{\"headers\":[\"Name\",\"Age\"], \"data\":[[\"Teresa\",\"5\"],[\"Daniel\",\"1\"]]}")
+                .post("/excel/1").peek().
+                then().assertThat()
+                .statusCode(405);
+    }
+
+    @Test
+    public void testExcelGenerationButIOExceptionRaised() throws IOException {
+        Mockito.when(excelService.saveRequest(any())).thenThrow(new IOException());
+        given().accept("application/json").contentType(ContentType.JSON)
+                .body("{\"headers\":[\"Name\",\"Age\"], \"data\":[[\"Teresa\",\"5\"],[\"Daniel\",\"1\"]]}")
+                .post("/excel").peek().
+                then().assertThat()
+                .statusCode(500);
+    }
+
+    @Test
+    public void testExcelGenerationButExcelFormatExceptionRaised() throws IOException {
+        Mockito.when(excelService.saveRequest(any())).thenThrow(new ExcelFormatException("data in one column should be of the same type"));
+        given().accept("application/json").contentType(ContentType.JSON)
+                .body("{\"headers\":[\"Name\",\"Age\"], \"data\":[[\"Teresa\",\"5\"],[\"Daniel\",\"1\"]]}")
+                .post("/excel").peek().
+                then().assertThat()
+                .statusCode(400)
+                .body("message", Matchers.equalTo("data in one column should be of the same type"));
     }
 }
--- src/test/java/com/antra/evaluation/reporting_system/ReportingSystemApplicationTests.java
+++ src/test/java/com/antra/evaluation/reporting_system/ReportingSystemApplicationTests.java
@@ -1,24 +1,35 @@
 package com.antra.evaluation.reporting_system;
 
-import com.antra.evaluation.reporting_system.pojo.report.ExcelData;
-import com.antra.evaluation.reporting_system.pojo.report.ExcelDataHeader;
-import com.antra.evaluation.reporting_system.pojo.report.ExcelDataSheet;
-import com.antra.evaluation.reporting_system.pojo.report.ExcelDataType;
+import com.antra.evaluation.reporting_system.pojo.api.ExcelRequest;
+import com.antra.evaluation.reporting_system.pojo.api.ExcelResponse;
+import com.antra.evaluation.reporting_system.pojo.report.*;
 import com.antra.evaluation.reporting_system.service.ExcelGenerationService;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.when;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ReportingSystemApplicationTests {
+
+    @Value("http://localhost:${local.server.port}")
+    private String REST_SERVICE_URI ;
 
     @Autowired
     ExcelGenerationService reportService;
@@ -69,14 +80,41 @@ class ReportingSystemApplicationTests {
         sheets.add(sheet2);
     }
 
+//    @Test
+//    public void testExcelGegeration() {
+//        File file = null;
+//        try {
+//            file = reportService.generateExcelReport(data);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        assertTrue(file != null);
+//    }
+
     @Test
     public void testExcelGegeration() {
-        File file = null;
-        try {
-            file = reportService.generateExcelReport(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        assertTrue(file != null);
+        ExcelRequest excelRequest = new ExcelRequest();
+        excelRequest.setHeaders(Arrays.asList("Student #","Name","Class","Score"));
+        excelRequest.setDescription("Student Math Course Report");
+        List<List<String>> data = new ArrayList<>();
+        data.add(Arrays.asList("s-001", "James", "Class-A", "A+"));
+        data.add(Arrays.asList("s-002","Robert","Class-A","A"));
+        data.add(Arrays.asList("s-003","Jennifer","Class-A","A"));
+        data.add(Arrays.asList("s-004","Linda","Class-B","B"));
+        data.add(Arrays.asList("s-005","Elizabeth","Class-B","B+"));
+        data.add(Arrays.asList("s-006","Susan","Class-C","A"));
+        data.add(Arrays.asList("s-007","Jessica","Class-C","A+"));
+        data.add(Arrays.asList("s-008","Sarah","Class-A","B"));
+        data.add(Arrays.asList("s-009","Thomas","Class-A","B-"));
+        data.add(Arrays.asList("s-010","Joseph","Class-B","A-"));
+        data.add(Arrays.asList("s-011","Charles","Class-C","A"));
+        data.add(Arrays.asList("s-012","Lisa","Class-D","B"));
+        excelRequest.setData(data);
+        RestTemplate restTemplate = new RestTemplate();
+        ExcelResponse excelResponse = restTemplate.postForObject(REST_SERVICE_URI+"/excel", excelRequest, ExcelResponse.class);
+        assertTrue(excelResponse.getFileId().equals("1"));
+        assertTrue(new File("1.xlsx").length() != 0);
     }
+
+
 }
